befb99f6576b0032939341a42e5e94d319fe050d
antonio gomez del peso <anseris51@Pgmail.com>
2018-12-13 19:53:54 +0100

    back

---
+++ bbdd.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS cursoAngular7;
+USE cursoAngular7;
+
+
+CREATE TABLE productos(
+id          int(255) auto_increment not null,
+nombre      varchar(255),
+description text,
+precio      varchar(255),
+imagen      varchar(255),
+CONSTRAINT pk_productos PRIMARY KEY(id)
+)ENGINE=InnoDb; 
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get("/pruebas", function() use($app){
+    echo "Hola mundo";
+});
+
+$app->run();
+
+?> 
